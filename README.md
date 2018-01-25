945ffcf2891381b9dc059c1458baf2a36a8a0d8f
deadcheat <deadcheat@users.noreply.github.com>
2018-01-25 18:50:05 +0900

    Feature/v0.1.0 add listen management (#1)
    * added failed test
    
    * fix failed test, implement all
    
    * fixed for older than go1.8

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ ___________
   |____|    \____/ |   __/  |__|    \____/ |   __/  \___  >
                    |__|                    |__|         \/
 ```
-
+# Toprope
 [![Build Status](https://travis-ci.org/deadcheat/toprope.svg?branch=master)](https://travis-ci.org/deadcheat/toprope) [![Coverage Status](https://coveralls.io/repos/github/deadcheat/toprope/badge.svg?branch=master)](https://coveralls.io/github/deadcheat/toprope?branch=master)
 
-# What's this
+## What's this
 
 toprope is aiming to flexible wrapper of net/http/test
 
@@ -20,7 +20,7 @@ but i want to specify a port or sometimes hostname too.
 
 that's why i created this lib. thank you.
 
-# How To Use
+## How To Use
 
 ```
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -35,5 +35,9 @@ that's why i created this lib. thank you.
 	ts.Start()
 	defer ts.Close()
 ```
+
+## Notice
+- toprope can't create TLS server now
+
 
 thankyou.
--- toprope.go
+++ toprope.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -14,8 +15,48 @@ const (
 	retryToListen = 3
 )
 
+// memo cache servers to manage close and listen
+var memo = make(map[string]*Server)
+
+// Server server instance used internal
+type Server struct {
+	i  *httptest.Server
+	wg *sync.WaitGroup
+}
+
+// URL access internal property
+func (s *Server) URL() string {
+	return s.i.URL
+}
+
+// Listener access internal property
+func (s *Server) Listener() net.Listener {
+	return s.i.Listener
+}
+
+// Config access internal property
+func (s *Server) Config() *http.Server {
+	return s.i.Config
+}
+
+// Close Override and call internal
+func (s *Server) Close() {
+	s.i.Close()
+	s.wg.Done()
+}
+
+// CloseClientConnections Override and call internal
+func (s *Server) CloseClientConnections() {
+	s.i.CloseClientConnections()
+}
+
+// Start Override and call internal
+func (s *Server) Start() {
+	s.i.Start()
+}
+
 // NewHttptestTCPServerFromURL generate *httptest.Server from url-string
-func NewHttptestTCPServerFromURL(urlstring string, handler http.Handler) (ts *httptest.Server, err error) {
+func NewHttptestTCPServerFromURL(urlstring string, handler http.Handler) (ts *Server, err error) {
 	u, err := url.Parse(urlstring)
 	if err != nil {
 		return
@@ -24,7 +65,7 @@ func NewHttptestTCPServerFromURL(urlstring string, handler http.Handler) (ts *ht
 }
 
 // NewHttptestTCPServer generates *httptest.Server from hostname and portnum
-func NewHttptestTCPServer(hostName string, port int, handler http.Handler) (ts *httptest.Server, err error) {
+func NewHttptestTCPServer(hostName string, port int, handler http.Handler) (ts *Server, err error) {
 	host := hostName
 	if port >= 0 {
 		host = fmt.Sprintf("%s:%d", hostName, port)
@@ -36,11 +77,17 @@ func NewHttptestTCPServer(hostName string, port int, handler http.Handler) (ts *
 	return listenAndCreateServer(u, handler)
 }
 
-func listenAndCreateServer(u *url.URL, handler http.Handler) (ts *httptest.Server, err error) {
+func listenAndCreateServer(u *url.URL, handler http.Handler) (ts *Server, err error) {
+	host := u.Host
+	s, ok := memo[host]
+	if ok && s.wg != nil {
+		// if memo has same hostname, wait listener to close certainly
+		s.wg.Wait()
+	}
 	var l net.Listener
 	// 規定回数リトライする
 	for index := 0; index < retryToListen; index++ {
-		l, err = net.Listen(modeTCP, u.Host)
+		l, err = net.Listen(modeTCP, host)
 		if err != nil {
 			time.Sleep(1 * time.Second)
 			continue
@@ -50,10 +97,16 @@ func listenAndCreateServer(u *url.URL, handler http.Handler) (ts *httptest.Serve
 	if err != nil {
 		return
 	}
-	ts = &httptest.Server{
-		URL:      u.String(),
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	server := &httptest.Server{
 		Listener: l,
 		Config:   &http.Server{Handler: handler},
 	}
+	ts = &Server{
+		i:  server,
+		wg: wg,
+	}
+	memo[host] = ts
 	return
 }
